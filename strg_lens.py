34f0bed6bea681c1ee701b6fa8a62b735ce180b8
Tansu Daylan <tansu.daylan@gmail.com>
2016-11-28 00:09:59 -0500

    NA

--- strg_lens.py
+++ strg_lens.py
@@ -4,26 +4,28 @@ def pcat_lens_mock():
     
     minmflux = deg2rad(1e-2 / 3600.)
     maxmflux = deg2rad(1e-1 / 3600.)
-    gridchan = pcat.main.init( \
-                              #verbtype=2, \
-                              numbswep=10000, \
-                              numbswepplot=10000, \
-                              factthin=100, \
-                              pntstype='lens', \
-                              exprinfo=False, \
-                              indxenerincl=arange(1), \
-                              indxevttincl=arange(1), \
-                              strgexpo=1e16, \
-                              datatype='mock', \
-                              exprtype='hubb', \
-                              stdvflux=0.01, \
-                              stdvlbhl=0.01, \
-                              maxmnumbpnts=array([20]), \
-                              minmflux=minmflux, \
-                              maxmflux=maxmflux, \
-                              mockbacp=zeros((1, 1)), \
-                              mocknumbpnts=array([10]), \
-                             )
+
+    for k in range(5):
+        gridchan = pcat.main.init( \
+                                  #verbtype=2, \
+                                  numbswep=100000, \
+                                  numbswepplot=10000, \
+                                  factthin=100, \
+                                  pntstype='lens', \
+                                  exprinfo=False, \
+                                  indxenerincl=arange(1), \
+                                  indxevttincl=arange(1), \
+                                  strgexpo=1e16, \
+                                  datatype='mock', \
+                                  exprtype='hubb', \
+                                  stdvflux=0.01, \
+                                  stdvlbhl=0.01, \
+                                  maxmnumbpnts=array([20]), \
+                                  minmflux=minmflux, \
+                                  maxmflux=maxmflux, \
+                                  mockbacp=zeros((1, 1)), \
+                                  mocknumbpnts=array([10]), \
+                                 )
 
 if len(sys.argv) > 1:
     name = globals().copy()
