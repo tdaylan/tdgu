3e2751314ffe882b2f9820a596339c08e83bddc4
Tansu Daylan <tansu.daylan@gmail.com>
2016-12-11 21:50:35 -0500

    NA

--- strg_lens.py
+++ strg_lens.py
@@ -48,7 +48,7 @@ def pcat_lens_mock_grid():
                                                 numbswepplot=2000, \
                                                 factthin=200, \
                                                 proplenp=False, \
-                                                makeplot=False, \
+                                                #makeplot=False, \
                                                 diagmode=False, \
                                                 proppsfp=False, \
                                                 exprinfo=False, \
