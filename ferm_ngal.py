b7fd21a0d9edba41686875323d8ef2eb4985eb82
Tansu Daylan <tansu.daylan@gmail.com>
2017-02-01 13:42:24 -0500

    NA

--- ferm_ngal.py
+++ ferm_ngal.py
@@ -91,10 +91,9 @@ def pcat_ferm_inpt_ngal_tim4():
 def pcat_ferm_mock_ngal():
      
     pcat.main.init( \
-                   numbswep=100000, \
-                   numbswepplot=5000, \
-                   factthin=1000, \
-                   proppsfp=False, \
+                   numbswep=300000, \
+                   numbswepplot=10000, \
+                   factthin=10000, \
                    indxenerincl=arange(1, 4), \
                    indxevttincl=arange(2, 4), \
                    lgalcntr=0., \
