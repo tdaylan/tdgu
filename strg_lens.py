ec140fba43812799d084e6dc480cb8b0ede26993
Tansu Daylan <tansu.daylan@gmail.com>
2017-04-15 12:48:50 -0400

    NA

--- strg_lens.py
+++ strg_lens.py
@@ -117,7 +117,7 @@ def pcat_lens_mock_syst():
     listlablinpt = ['Nominal', '$N=1$', r'$\alpha_{s,min}$', 'Long']
     dictargsvari = {}
     dictargsvari['numbswep'] = [numbswepnomi, numbswepnomi, numbswepnomi, 10*numbswepnomi]
-    dictargsvari['fittminmdefs'] = [None, 1e-2/3600./180.*pi, None, None]
+    dictargsvari['fittminmdefs'] = [None, 3e-2/3600./180.*pi, None, None]
     dictargsvari['fittminmnumbpnts'] = [None, array([1]), None, None]
     dictargsvari['fittmaxmnumbpnts'] = [None, array([1]), None, None]
     
@@ -155,17 +155,43 @@ def pcat_lens_mock_intr():
                       )
     
 
+def pcat_lens_mock_cova():
+   
+    numbiter = 10
+    for k in range(numbiter):
+        pcat.main.init( \
+                       numbswep=50000, \
+                       #intreval=True, \
+                       factthin=1000, \
+                       makeplotintr=True, \
+                       condcatl=False, \
+                       pertmodleval=False, \
+                       propwithsing=False, \
+                       #shrtfram=True, \
+                       #makeplot=False, \
+                       #checprio=True, \
+                       #trueminmbacpbac0ene0=1e5, \
+                       #truemaxmbacpbac0ene0=1e7, \
+                       #truebacpbac0ene0=1e6, \
+                       elemtype='lens', \
+                       inittype='refr', \
+                       exprtype='hubb', \
+                       truenumbpnts=array([30]), \
+                       truemaxmnumbpnts=array([60]), \
+                      )
+    
 def pcat_lens_mock():
    
     numbiter = 10
     for k in range(numbiter):
         pcat.main.init( \
-                       numbswep=100000, \
+                       numbswep=10000, \
                        #intreval=True, \
                        factthin=1000, \
                        makeplotintr=True, \
                        condcatl=False, \
                        #shrtfram=True, \
+                       #makeplot=False, \
                        #checprio=True, \
                        #trueminmbacpbac0ene0=1e5, \
                        #truemaxmbacpbac0ene0=1e7, \
