8a4abcbd48f7f140674343bcb4902fc2a7030819
Tansu Daylan <tansu.daylan@gmail.com>
2017-09-26 08:47:52 -0400

    NA

--- xray_back.py
+++ xray_back.py
@@ -59,7 +59,7 @@ def writ_chan():
                     if datatype == 'home':
                         for i in indxener:
                             # count map
-                            path = '/n/fink1/rfeder/obsids/full/merged_%dMs/rest_fov/%d/%s-%s_flux.img' % (expomaps[k], i, strgener[i], strgener[i+1])
+                            path = '/n/fink1/rfeder/xray_pcat/obsids/full/merged_%dMs/rest_fov/%d/%s-%s_flux.img' % (expomaps[k], i, strgener[i], strgener[i+1])
                             temp = pf.getdata(path, 0)
                     
                     numbsideyaxi = temp.shape[0]
@@ -137,7 +137,7 @@ def writ_chan():
                     strgvarbmine = ['expo', 'sbrt']
                     for i in indxener:
                         for a in range(2):
-                            path = '/n/fink1/rfeder/obsids/full/merged_%dMs/rest_fov/%d/%s-%s_%s' % (expomaps[k], i, strgener[i], strgener[i+1], strgvarb[a])
+                            path = '/n/fink1/rfeder/xray_pcat/obsids/full/merged_%dMs/rest_fov/%d/%s-%s_%s' % (expomaps[k], i, strgener[i], strgener[i+1], strgvarb[a])
                             if a == 0:
                                 expo[i, :, :, 0] = pf.getdata(path, 0)[minmindx[0]:maxmindx[0], minmindx[1]:maxmindx[1]]
                             if a == 1:
