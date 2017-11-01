e58d32802359b514fba73bebe8a973f59c229615
Tansu Daylan <tansu.daylan@gmail.com>
2017-11-01 18:16:25 -0400

    NA

--- subm_auto.py
+++ subm_auto.py
@@ -4,7 +4,11 @@ path = os.environ["TDGU_PATH"] + '/'
 pathfileoutp = path + 'subm_auto.log'
 os.system('rm %s' % pathfileoutp)
 fileoutp = open(pathfileoutp, 'w')
-cntr = 0
+               
+# list of PCAT run plot outputs
+pathimag = os.environ["PCAT_DATA_PATH"] + '/imag/'
+listrtag = fnmatch.filter(os.listdir(pathimag), '2*')
+
 for name in os.listdir(path):
     if name.endswith(".py"):
         print name
@@ -12,11 +16,23 @@ for name in os.listdir(path):
         for line in fileobjt:
             if line.startswith('def pcat_'):
                 
-                #if cntr == 5:
-                #    break
-                
-                cntr += 1
                 namefunc = line[4:-1].split('(')[0]
+                
+                print 'Function %s' % namefunc
+                    
+                # check the available run outputs
+                booltemp = False
+                for rtag in listrtag:
+                    strgstat = os.environ["PCAT_DATA_PATH"] + '/data/outp/' + rtag
+                    if rtag.startswith(namefunc) and os.path.isfile(strgstat):
+                        filestat = open(strgstat, 'r')
+                        for line in filestat:
+                            if line == 'gdatfinl written.\n':
+                                booltemp = True
+                if booltemp:
+                    print 'Found a previously completed run.'
+                    continue
+
                 cmnd = 'python $TDGU_PATH/%s %s' % (name, namefunc)
                 print cmnd
                 try:
