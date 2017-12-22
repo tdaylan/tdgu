be8b0508b4b65448f1f7d8287291401c87a0acab
Tansu Daylan <tansu.daylan@gmail.com>
2017-12-22 12:23:04 -0500

    NA

--- xray_back.py
+++ xray_back.py
@@ -181,7 +181,12 @@ def writ_chan():
                         indxtemp = where(expo[i, :, :, 0] > 0.)
                         sbrt[i, indxtemp[0], indxtemp[1], 0] = cntp[i, indxtemp[0], indxtemp[1], 0] / expo[i, indxtemp[0], indxtemp[1], 0] / diffener[i] / apix
                     
-                    if False:
+                    print 'heeey'
+                    print 'indxener'
+                    print indxener
+                    print
+
+                    if True:
                         for i in indxener:
                             print 'i'
                             print i
@@ -191,6 +196,13 @@ def writ_chan():
                             summgene(cntp[i, :, :, 0])
                             print 'sbrt[i, :, :, 0]'
                             summgene(sbrt[i, :, :, 0])
+                            print
+                            if amax(cntp[i, :, :, 0]) == 0:
+                                print 'datatype'
+                                print datatype
+                                print 'indxener'
+                                print indxener
+                                raise Exception('')
 
                     pathdatapcat = os.environ["PCAT_DATA_PATH"] + '/data/inpt/'
                     
