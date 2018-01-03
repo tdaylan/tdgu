2d682ddb06b2be5279e3774059d68771ed408fa3
Tansu Daylan <tansu.daylan@gmail.com>
2018-01-03 17:10:36 -0500

    NA

--- xray_back.py
+++ xray_back.py
@@ -372,7 +372,8 @@ def pcat_chan_mock(strgcnfgextnexec=None):
     dictargs['priofactdoff'] = 0.2
     
     dictargs['optitype'] = 'none'
-    dictargs['numbswep'] = 100000
+    dictargs['numbswep'] = 1000
+    dictargs['numbsamp'] = 10
     
     listnamecnfgextn = ['nomi', 'truevlow', 'trueloww', 'truehigh', 'truenone']
     dictargsvari = {}
