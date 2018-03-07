89165688ab9d065aaa5130537aec98317a40afec
Tansu Daylan <tansu.daylan@gmail.com>
2018-03-07 14:42:08 -0500

    NA

--- subm_auto.py
+++ subm_auto.py
@@ -5,7 +5,7 @@ pathfileoutp = path + 'subm_auto_%s.log' % tdpy.util.retr_strgtimestmp()
 os.system('rm %s' % pathfileoutp)
 fileoutp = open(pathfileoutp, 'w')
                
-
+listnamefunc = []
 for name in os.listdir(path):
     if name.endswith(".py"):
         print name
@@ -15,19 +15,22 @@ for name in os.listdir(path):
                 
                 namefunc = line[4:-1].split('(')[0]
                 
-                print 'Processing confugiration %s...' % namefunc
-                    
-                cmnd = 'python $TDGU_PATH/%s %s' % (name, namefunc)
-                print cmnd
-                try:
-                    #os.system(cmnd)
-                    if namefunc == 'pcat_lens_mock_trueminmdefs':
-                        raise Exception('')
-                except Exception as excp:
-                    strg = str(excp)
-                    fileoutp.write('%s failed.\n' % namefunc)
-                    fileoutp.write(strg)
-                print
+                listnamefunc.append(namefunc)
+
+for namefunc in array(listnamefunc)[choice(arange(len(listnamefunc)), size=len(listnamefunc), replace=False)]:
+    print 'Processing confugiration %s...' % namefunc
+        
+    cmnd = 'python $TDGU_PATH/%s %s' % (name, namefunc)
+    print cmnd
+    try:
+        os.system(cmnd)
+        if namefunc == 'pcat_lens_mock_trueminmdefs':
+            raise Exception('')
+    except Exception as excp:
+        strg = str(excp)
+        fileoutp.write('%s failed.\n' % namefunc)
+        fileoutp.write(strg)
+    print
 
 fileoutp.close()
 
