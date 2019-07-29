82d5f4a1200529f857c07fba0af6760efbe4d139
Tansu Daylan <tansu.daylan@gmail.com>
2019-07-28 22:20:38 -0400

    N/A

--- wasp121b/main.py
+++ wasp121b/main.py
@@ -8,6 +8,7 @@ from scipy.signal import lombscargle
 
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
+import matplotlib
 
 from astropy import constants as c
 
@@ -27,6 +28,12 @@ from exoworlds.tess import extract_SPOC_data, extract_QLP_data
 
 import emcee
 import dynesty
+
+matplotlib.rc('text', usetex = True)
+matplotlib.rc('font', **{'family' : "sans-serif"})
+params= {'text.latex.preamble' : [r'\usepackage{amsmath}']}
+plt.rcParams.update(params)
+
 
 def retr_lpos(para, meanphas, data, datastdv, modltype, indxoccl):
     
@@ -49,11 +56,27 @@ def retr_lpos(para, meanphas, data, datastdv, modltype, indxoccl):
     return lpos
 
 
-def retr_modl(meanphas, offs, amfo, tempdayy, tempnigh, shft, modltype, indxoccl):
+def retr_modl(meanphas, temp):
     
     phasshft = (meanphas - shft * np.pi / 180.) * 2. * np.pi
     occl = retr_occl(tempdayy, tempstar)
     deptnigh = retr_occl(tempnigh, tempstar)
+    deptatmo = occl - deptnigh
+    modl = offs + 0.5 * 1e-6 * deptatmo * np.cos(phasshft)
+    for k in range(3):
+        modl += 0.5e-6 * amfo[k] * np.sin((k + 1) * meanphas * 2. * np.pi) 
+    for k in range(2):
+        modl += 0.5e-6 * amfo[k+5] * np.cos((k + 2) * meanphas * 2. * np.pi)
+    
+    modl[indxoccl] -= occl * 1e-6
+    
+    return modl, deptatmo
+
+
+def retr_modl(gdat, offs, amfo, shft, modltype):
+    
+    phasshft = (meanphas - shft * np.pi / 180.) * 2. * np.pi
+    occl = retr_occl(tempdayy, tempstar)
     deptatmo = occl - deptnigh
     modl = offs + 0.5 * 1e-6 * deptatmo * np.cos(phasshft)
     for k in range(3):
@@ -119,10 +142,9 @@ def retr_mask(time, flag, strgmask):
     return indx
 
 # paths
-pathbase = '/Users/tdaylan/DropboxMITTTT/wasp121b/tansu/'
-pathstor = pathbase + 'shft/stor/'
-pathdata = '/Users/tdaylan/Desktop/temp/WASP-121/data/'
-pathimag = pathstor + 'imag/'
+pathbase = os.environ['DATA'] + '/wasp0121/'
+pathdata = pathbase + 'data/'
+pathimag = pathbase + 'imag/'
 
 # parameters
 # WASP-121
@@ -153,8 +175,18 @@ time, lcursapp, stdvlcursapp = np.genfromtxt(pathdata + 'data_preparation/TESS_S
 
 
 
+figrsize = np.empty((5, 2))
+figrsize[0, :] = np.array([12., 4.])
+figrsize[1, :] = np.array([12., 6.])
+figrsize[2, :] = np.array([12., 10.])
+figrsize[3, :] = np.array([12., 14.])
+figrsize[4, :] = np.array([6., 6.])
+boolpost = True
+if boolpost:
+    figrsize /= 1.5
+
 # plot light curves
-figr, axis = plt.subplots(2, 1, figsize=(12, 5))
+figr, axis = plt.subplots(2, 1, figsize=figrsize[1, :])
 axis[0].plot(time - 2457000, lcurpdcc, 'k.')
 axis[1].plot(time - 2457000, lcursapp, '.', color='k')
 for a in range(2):
@@ -166,7 +198,7 @@ plt.savefig(path)
 plt.close()
 
 # fit a spline to the SAP light curve
-figr, axis = plt.subplots(2, 1, figsize=(12, 5))
+figr, axis = plt.subplots(2, 1, figsize=figrsize[1, :])
 timebrek = [0., 2458504., np.inf]
 numbbrek = len(timebrek)
 lcurdetr = []
@@ -185,7 +217,7 @@ for i in np.arange(numbbrek - 1):
     print '$t_k$:', spl.get_knots()
     xx = np.linspace(time1[0], time1[-1], 1000)
     #axis[0].plot(time1 - 2457000, flux1, 'k.')
-    axis[0].plot(time1[ind_out] - 2457000, flux1[ind_out], ls='', color='black')
+    axis[0].plot(time1[ind_out] - 2457000, flux1[ind_out], 'k.', color='black')
     axis[0].plot(xx - 2457000, spl(xx)+1., 'm-' )
     axis[1].plot(time1 - 2457000, flux1-spl(time1), '.', color='black')
     lcurdetr += list(flux1-spl(time1))
@@ -210,28 +242,15 @@ plt.close()
 phasfold, pcur, stdvpcur, N, phas = phase_fold(time, lcurdetr, peri, epoc, dt = 0.005, ferr_type='meansig', ferr_style='sem', sigmaclip=True)
 #phasfold, pcur, stdvpcur, N, phas = phase_fold(time, lcurdetr, peri, epoc, dt=0.001, ferr_type='meansig', ferr_style='sem', sigmaclip=True)
 #phasfold, pcur, stdvpcur, N, phas = phase_fold(time, lcurdetr, peri, epoc, dt=0.001/5., ferr_type='meansig', ferr_style='sem', sigmaclip=True)
-figr, axis = plt.subplots(2, 1, figsize=(12, 8))
-axis[0].plot(phas, lcurdetr, '.', color='grey', alpha=0.3)
-axis[0].plot(phasfold, pcur, 'k.')
-axis[1].plot(phas, lcurdetr, '.', color='grey', alpha=0.3)
-axis[1].plot(phasfold, (pcur - 1) * 1e6, 'k.', ms=10)
-axis[0].set(ylabel='Relative Flux')
-axis[1].set(ylabel='Relative Flux - 1 [ppm]')
-axis[1].set(ylim=[-800, 1000])
-axis[0].set_xticklabels([])
-axis[1].set_xlabel('Phase')
-plt.subplots_adjust(hspace=0.)
-path = pathimag + 'pcur.pdf'
-plt.savefig(path)
-plt.close()
 
 data = np.column_stack((time, lcurdetr, stdvlcursapp))
 np.savetxt(pathdata + 'data_preparation/TESS.csv', data, delimiter=',', header='time,flux,flux_err')
 
-figr, axis = plt.subplots(figsize=(12, 5))
+figr, axis = plt.subplots(figsize=figrsize[0, :])
 axis.plot(phas, lcurdetr, '.', color='grey', alpha=0.3)
-axis.errorbar(phasfold, pcur, yerr=stdvpcur, ls='', fmt='black', capsize=0, ms=8)
+axis.errorbar(phasfold, pcur, yerr=stdvpcur, ls='', marker='o', color='black', capsize=0, ms=8)
 axis.set(xlabel='Phase', ylabel='Flux')
+plt.tight_layout()
 path = pathimag + 'pcur.pdf'
 plt.savefig(path)
 plt.close()
@@ -240,7 +259,8 @@ plt.close()
 companion = 'b'
 inst = 'TESS'
 key = 'flux'
-pathalle = pathdata + 'allesfits/allesfit_full_gp/'
+#pathalle = pathdata + 'allesfits/allesfit_full_gp/'
+pathalle = pathdata + 'allesfits/allesfit_orbit_2/'
 alles = allesfitter.allesclass(pathalle)
 
 radirati = alles.posterior_params['b_rr']
@@ -259,7 +279,7 @@ listsamp[:, 1] = (sbrgrati + fluxrati) * 1e6
 listsamp[:, 2] = fluxrati * 1e6
 
 listlabl = ['Nightside [ppm]', 'Secondary [ppm]', 'Modulation [ppm]']
-tdpy.mcmc.plot_grid(pathimag, 'post', listsamp, listlabl)
+tdpy.mcmc.plot_grid(pathimag, 'post', listsamp, listlabl, plotsize=figrsize[0, 1])
 
 # plot a lightcurve from the posteriors
 time = np.concatenate((alles.data['TESS']['time'], alles.data['TESS']['time']+alles.posterior_params_median['b_period']))
@@ -269,7 +289,7 @@ model_flux = alles.get_posterior_median_model(inst, key, xx=model_time)
 baseline = alles.get_posterior_median_baseline(inst, key, xx=time)
 bintime, binflux, binflux_err, N = rebin_err(time, flux-baseline, ferr=None, dt = 0.02, phasefolded=False, ferr_type='meansig', ferr_style='sem', sigmaclip=True)
 
-figr, axis = plt.subplots(3, 1, figsize=(12, 14))
+figr, axis = plt.subplots(3, 1, figsize=figrsize[3, :])
 for k in range(len(axis)):
     if k == 0:
         varbtemp = flux-baseline
@@ -296,21 +316,6 @@ axis[1].set(ylim=[-800,1000])
 axis[2].set(xlim=[0.2*peri,0.8*peri], ylim=[-800, 1000])
 
 alles = allesfitter.allesclass(pathalle)
-alles.posterior_params_median['b_sbratio_TESS'] = 0
-alles.settings['host_shape_TESS'] = 'sphere'
-alles.settings['b_shape_TESS'] = 'sphere'
-alles.posterior_params_median['b_geom_albedo_TESS'] = 0
-alles.posterior_params_median['host_gdc_TESS'] = 0
-alles.posterior_params_median['host_bfac_TESS'] = 0
-model_flux2 = alles.get_posterior_median_model(inst, key, xx=model_time)
-for k in range(len(axis)):
-    if k == 0:
-        varbtemp = model_flux2
-    else:
-        varbtemp = (model_flux2 - 1.) * 1e6
-    axis[k].plot(model_time, varbtemp, lw=2, color='m', label='PT', ls='--')
-
-alles = allesfitter.allesclass(pathalle)
 #alles.posterior_params_median['b_sbratio_TESS'] = 0
 alles.settings['host_shape_TESS'] = 'sphere'
 alles.settings['b_shape_TESS'] = 'sphere'
@@ -323,7 +328,7 @@ for k in range(len(axis)):
         varbtemp = model_flux2
     else:
         varbtemp = (model_flux2 - 1.) * 1e6
-    axis[k].plot(model_time, varbtemp, lw=2, color='g', label='PIL', ls='--', zorder=11)
+    axis[k].plot(model_time, varbtemp, lw=2, color='g', label='Nightside', ls='--', zorder=11)
 
 alles = allesfitter.allesclass(pathalle)
 alles.posterior_params_median['b_sbratio_TESS'] = 0
@@ -338,7 +343,7 @@ for k in range(len(axis)):
         varbtemp = model_flux2
     else:
         varbtemp = (model_flux2 - 1.) * 1e6
-    axis[k].plot(model_time, varbtemp, lw=2, color='orange', ls='--', label='SGD', zorder=11)
+    #axis[k].plot(model_time, varbtemp, lw=2, color='orange', ls='--', label='SGD', zorder=11)
 
 alles = allesfitter.allesclass(pathalle)
 alles.posterior_params_median['b_sbratio_TESS'] = 0
@@ -353,7 +358,7 @@ for k in range(len(axis)):
         varbtemp = model_flux2
     else:
         varbtemp = (model_flux2 - 1.) * 1e6
-    axis[k].plot(model_time, varbtemp, lw=2, color='r', ls='--', label='SEV', zorder=11)
+    axis[k].plot(model_time, varbtemp, lw=2, color='r', ls='--', label='Ellipsoidal', zorder=11)
 
 alles = allesfitter.allesclass(pathalle)
 alles.posterior_params_median['b_sbratio_TESS'] = 0
@@ -379,7 +384,7 @@ plt.close()
 
 
 # plot the spherical limits
-figr, axis = plt.subplots(2, 1, figsize=(12, 10))
+figr, axis = plt.subplots(2, 1, figsize=figrsize[2, :])
 
 alles.settings['host_shape_TESS'] = 'roche'
 alles.settings['b_shape_TESS'] = 'roche'
@@ -479,7 +484,13 @@ wlen = np.loadtxt(path)
 #  5. IRAC 4.5 micron
 #  6-22. WFC3 G102
 #  23-50. WFC3 G141
-figr, axis = plt.subplots(4, 1, figsize=(12, 14))
+listcolr = ['k', 'm', 'purple', 'olive', 'olive', 'r', 'g']
+for i in range(15):
+    listcolr.append('r')
+for i in range(28):
+    listcolr.append('g')
+listcolr
+figr, axis = plt.subplots(4, 1, figsize=figrsize[3, :], sharex=True)
 path = pathdata + 'ascii_output/EmissionModelArray.txt'
 arrymodl = np.loadtxt(path)
 print 'arrymodl'
@@ -492,39 +503,45 @@ meanlambband = data[:, 0] * 1e-3
 thptband = data[:, 1] 
 
 ## stellar spectrum and TESS throughput
-axis[0].plot(arrymodl[:, 0], 1e-9 * arrymodl[:, 9], label='Star', color='k')
+axis[0].plot(arrymodl[:, 0], 1e-9 * arrymodl[:, 9], label='Host star, WASP-121', color='grey')
+axis[0].plot(0., 0., ls='--', label='TESS Throughput', color='grey')
 axis[0].set_ylabel(r'$F_{\nu}$ [10$^9$ erg/s/cm$^2$]')
+#axis[0].legend(fancybox=True, loc=1)
+axis[0].legend(fancybox=True, bbox_to_anchor=[0.6, 0.2, 0.2, 0.2])
 axistwin = axis[0].twinx()
-axistwin.plot(meanlambband, thptband, color='grey')
-axistwin.set_ylabel(r'TESS Throughput')
+axistwin.plot(meanlambband, thptband, color='grey', ls='--', label='TESS')
+axistwin.set_ylabel(r'Throughput')
 
 ## eclipse depths
 ### model
 axis[1].plot(arrymodl[:, 0], arrymodl[:,1], label='Retrieval')
-axis[1].plot(arrymodl[:, 0], arrymodl[:,2], label='Blackbody', color='orange')
-axis[1].fill_between(arrymodl[:, 0], arrymodl[:, 3], arrymodl[:, 4], alpha=0.1, color='orange')
+axis[1].plot(arrymodl[:, 0], arrymodl[:,2], label='Blackbody', alpha=0.3, color='skyblue')
+axis[1].fill_between(arrymodl[:, 0], arrymodl[:, 3], arrymodl[:, 4], alpha=0.3, color='skyblue')
 ### data
-for k in range(8):
-    labl = None
-    axis[1].errorbar(arrydata[k, 0], arrydata[k, 2], xerr=arrydata[k, 1], yerr=arrydata[k, 3], label=labl, color=None, ls='', marker='o')
+for k in range(5):
+    axis[1].errorbar(arrydata[k, 0], arrydata[k, 2], xerr=arrydata[k, 1], yerr=arrydata[k, 3], ls='', marker='o', color=listcolr[k])
+axis[1].errorbar(arrydata[5:22, 0], arrydata[5:22, 2], xerr=arrydata[5:22, 1], yerr=arrydata[5:22, 3], ls='', marker='o', color='r')
+axis[1].errorbar(arrydata[22:, 0], arrydata[22:, 2], xerr=arrydata[22:, 1], yerr=arrydata[22:, 3], ls='', marker='o', color='g')
 axis[1].set_ylabel(r'Depth [ppm]')
 axis[1].set_xticklabels([])
 
 ## spectra
 ### model
-axis[2].plot(arrymodl[:, 0], 1e-9 * arrymodl[:, 5], label='Retrieval', color='b')
-axis[2].plot(arrymodl[:, 0], 1e-9 * arrymodl[:, 6], label='Blackbody', color='skyblue', alpha=0.3)
-axis[2].fill_between(arrymodl[:, 0], 1e-9 * arrymodl[:, 7], 1e-9 * arrymodl[:, 8], color='skyblue', alpha=0.3)
+objtplotretr, = axis[2].plot(arrymodl[:, 0], 1e-9 * arrymodl[:, 5], label='Retrieval', color='b')
+objtplotmblc, = axis[2].plot(arrymodl[:, 0], 1e-9 * arrymodl[:, 6], label='Blackbody', color='skyblue', alpha=0.3)
+objtploteblc = axis[2].fill_between(arrymodl[:, 0], 1e-9 * arrymodl[:, 7], 1e-9 * arrymodl[:, 8], color='skyblue', alpha=0.3)
+axis[2].legend([objtplotretr, (objtplotmblc, objtploteblc)], ['Retrieval', 'Blackbody'], loc=1)
 ### data
-for k in range(8):
-    axis[2].errorbar(arrydata[k, 0],  1e-9 * arrydata[k, 2], xerr=arrydata[k, 1], yerr=1e-9*arrydata[k, 3], \
-                                                                                        label=None, color=None, ls='', marker='o')
+for k in range(5):
+    axis[2].errorbar(arrydata[k, 0],  1e-9 * arrydata[k, 6], xerr=arrydata[k, 1], yerr=1e-9*arrydata[k, 7], ls='', marker='o', color=listcolr[k])
+axis[2].errorbar(arrydata[5:22, 0], 1e-9 * arrydata[5:22, 6], xerr=arrydata[5:22, 1], yerr=1e-9*arrydata[5:22, 7], ls='', marker='o', color='r')
+axis[2].errorbar(arrydata[22:, 0], 1e-9 * arrydata[22:, 6], xerr=arrydata[22:, 1], yerr=1e-9*arrydata[22:, 7], ls='', marker='o', color='g')
 axis[2].set_ylabel(r'$F_{\nu}$ [10$^9$ erg/s/cm$^2$]')
 axis[2].set_xticklabels([])
 
 ## brightness temperature
 ### data
-for k in range(8):
+for k in range(5):
     if k == 0:
         labl = 'TESS (This work)'
     if k == 1:
@@ -535,23 +552,20 @@ for k in range(8):
         labl = 'IRAC 3.6 $\mu$m'
     if k == 4:
         labl = 'IRAC 4.5 $\mu$m'
-    if k == 5:
-        labl = 'HST/WFC3 G102'
-    if k == 6:
-        labl = 'HST/WFC3 G141'
-    else:
-        labl = None
-#  1. TESS
-#  2. Zprime (ground photometry)
-#  3. Ks (ground photometry)
-#  4. IRAC 3.6 micron
-#  5. IRAC 4.5 micron
-#  6-22. WFC3 G102
-#  23-50. WFC3 G141
-    axis[3].errorbar(arrydata[k, 0], arrydata[k, 4], xerr=arrydata[k, 1], yerr=arrydata[k, 5], label=labl, color=None, ls='', marker='o')
+    axis[3].errorbar(arrydata[k, 0], arrydata[k, 4], xerr=arrydata[k, 1], yerr=arrydata[k, 5], label=labl, ls='', marker='o', color=listcolr[k])
+axis[3].errorbar(arrydata[5:22, 0], arrydata[5:22, 4], xerr=arrydata[5:22, 1], yerr=arrydata[5:22, 5], label='HST G102', ls='', marker='o', color='r')
+axis[3].errorbar(arrydata[22:, 0], arrydata[22:, 4], xerr=arrydata[22:, 1], yerr=arrydata[22:, 5], label='HST G141', ls='', marker='o', color='g')
 axis[3].set_ylabel(r'$T_B$ [K]')
 axis[3].set_xlabel(r'$\lambda$ [$\mu$m]')
-axis[3].legend(fancybox=True, bbox_to_anchor=[0.8, 0.8, 0.2, 0.2])
+axis[3].legend(fancybox=True, bbox_to_anchor=[0.8, 2.47, 0.2, 0.2], ncol=2)
+
+axis[1].set_ylim([20, None])
+axis[1].set_yscale('log')
+for i in range(4):
+    axis[i].set_xscale('log')
+axis[3].set_xlim([0.5, 5])
+axis[3].xaxis.set_minor_formatter(matplotlib.ticker.ScalarFormatter())
+axis[3].xaxis.set_major_formatter(matplotlib.ticker.ScalarFormatter())
 plt.subplots_adjust(hspace=0., wspace=0.)
 path = pathimag + 'spec.pdf'
 print 'Writing to %s...' % path
@@ -576,7 +590,7 @@ for strg in liststrgcomp:
     listdatacomp.append(np.loadtxt(path))
 
 ## contibution/PT/abun
-figr = plt.figure(figsize=(12, 4))
+figr = plt.figure(figrsize[0, :])
 gs = gridspec.GridSpec(1, 2, width_ratios=[1, 2])
 axis = [[] for a in range(2)]
 for a in range(2):
@@ -615,7 +629,7 @@ plt.close()
 
 
 # cross-section
-figr, axis = plt.subplots(figsize=(6, 4))
+figr, axis = plt.subplots(figrsize[4, :])
 for k in indxcomp:
     print 'k'
     print k
@@ -624,7 +638,7 @@ for k in indxcomp:
     print 'listdatacomp[k][:, 0]'
     summgene(listdatacomp[k][:, 0])
     #axis.plot(listdatacomp[k][:, 1], listdatacomp[k][:, 0])
-axis.set_xlabel('$\lambda$ [$\mu$m]$')
+axis.set_xlabel('$\lambda$ [$\mu$m]')
 axis.set_ylabel('$\sigma$ [cm$^2$]')
 axis.set_yscale('log')
 plt.subplots_adjust()
@@ -655,14 +669,13 @@ listaminchi2runs = []
 listsamppararuns = []
 listlistlablpara = []
 listlistlablparafull = []
-liststrgruns = []
 listmodltyperuns = []
     
 # get data
 path = pathdata + 'data_preparation/TESS.csv'
 arryfrst = np.loadtxt(path, delimiter=',', skiprows=1)
     
-arrythrd = tesstarg.util.fold(arryseco, epoc, peri)
+arrythrd = tesstarg.util.fold(arryfrst, epoc, peri)
     
 # parse the data
 meanphas = (arrythrd[:, 0] - epoc) / peri
@@ -676,6 +689,7 @@ data = data[indxsort]
 datastdv = datastdv[indxsort]
 
 # mask out the primary transit
+timetole = 6. / 24.
 indx = np.where(abs(meanphas - 0.5) < (1. - timetole / peri) / 2.)[0]
 data = data[indx]
 datastdv = datastdv[indx]
@@ -694,17 +708,6 @@ for modltype in listmodltype:
         numbpara = 11
     
     listmodltyperuns.append('%s' % modltype)
-    if strgbins != 'ubnd':
-        strgruns = 'binned'
-    if modltype == 'umsk':
-        strgruns = 'no mask'
-    if modltype == 'ful1':
-        strgruns = 'first orbit'
-    if modltype == 'ful1':
-        strgruns = 'second orbit'
-    if modltype == 'shft':
-        strgruns = 'phase offset'
-    liststrgruns.append(strgruns)
     if samptype == 'emce':
         numbsamp = 10000
         numbsampburn = 1000
@@ -877,7 +880,7 @@ for modltype in listmodltype:
             shft = np.zeros_like(tempdayy)
         phasmodlfine[k, :], deptatmofine = retr_modl(meanphasfine, offs, amfo, tempdayy, tempstar, shft, modltype, indxphasfineoccl)
     
-    figr, axis = plt.subplots(figsize=(12, 4))
+    figr, axis = plt.subplots(figrsize[0, :])
     axis.errorbar(meanphas, data, yerr=yerr, color='black', marker='o', ls='', alpha=0.2, markersize=1)
     for k, indxsampplottemp in enumerate(indxsampplot):
         axis.plot(meanphasfine, phasmodlfine[k, :], alpha=0.05, color='b')
@@ -889,45 +892,33 @@ for modltype in listmodltype:
     plt.savefig(path)
     plt.close()
     
-indxruns = len(liststrgruns)
 indxruns = np.arange(numbruns)
 for b in indxruns:
     tdpy.mcmc.plot_grid(pathimag, '%d' % b, listsamppararuns[b], listlistlablparafull[b])
 
 fileoutp = open(pathdata + 'post.csv', 'w')
 fileoutp.write(' & ')
-for k in indxrunsdivithis:
-    b = a * numbrrunsdivi + k
-    fileoutp.write('%s' % liststrgruns[b])
-    if k != numbrunsdivithis - 1:
-        fileoutp.write(' & ')
+fileoutp.write(' & ')
 fileoutp.write('\\\\\n')
 fileoutp.write('\\hline\n')
 fileoutp.write('\\hline\n')
 fileoutp.write('\\hline\n')
 fileoutp.write('$\chi^2_{\\nu}$ & ')
-for k in indxrunsdivithis:  
-    b = a * numbrrunsdivi + k
-    fileoutp.write('$%.3g$' % listaminchi2runs[b])
-    if k != numbrunsdivithis - 1:
-        fileoutp.write(' & ')
+fileoutp.write('$%.3g$' % listaminchi2runs[b])
+fileoutp.write(' & ')
 fileoutp.write('\\hline\n')
 fileoutp.write('\\hline\n')
 fileoutp.write('\\\\\n')
 for n in indxparafull:
     fileoutp.write('%s & ' % listlablparafull[n])
-    for k in indxrunsdivithis:
-        b = a * numbrrunsdivi + k
-        if len(listlistlablparafull[b]) < numbparafull:
-            continue
-        ydat = np.median(listsamppararuns[b][:, n])
-        uerr = np.percentile(listsamppararuns[b][:, n], 84.) - ydat
-        lerr = ydat - np.percentile(listsamppararuns[b][:, n], 16.)
-        fileoutp.write('$%.3g \substack{+%.3g \\\\ -%.3g}$' % (ydat, uerr, lerr))
-        if k != numbrunsdivithis - 1:
-            fileoutp.write(' & ')
+    if len(listlistlablparafull[b]) < numbparafull:
+        continue
+    ydat = np.median(listsamppararuns[b][:, n])
+    uerr = np.percentile(listsamppararuns[b][:, n], 84.) - ydat
+    lerr = ydat - np.percentile(listsamppararuns[b][:, n], 16.)
+    fileoutp.write('$%.3g \substack{+%.3g \\\\ -%.3g}$' % (ydat, uerr, lerr))
+    fileoutp.write(' & ')
     fileoutp.write('\\\\\n')
 fileoutp.write('\\hline\n')
 fileoutp.close()
-
 
