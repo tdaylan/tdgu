c238bd36c25f4a0ca3af329a3a5d6eeb29136588
Tansu Daylan <tansu.daylan@gmail.com>
2020-02-12 15:56:49 -0500

    NA

--- pcurcorr/main.py
+++ pcurcorr/main.py
@@ -1,4 +1,7 @@
 from tdpy.util import summgene
+import tdpy.util
+import tdpy.mcmc
+import tesstarg.util
 
 import pandas as pd
 
@@ -9,27 +12,52 @@ import matplotlib.pyplot as plt
 
 import os
 
+def retr_modl(gdat, para, inpt):
+    
+    slop = para[0]
+    intc = para[1]
+    
+    line = inpt * slop + intc
+    
+    return line, []
+
+
+def retr_llik(gdat, para):
+   
+    slop = para[0]
+    intc = para[1]
+    dist = abs(-slop * gdat.tempinpt + gdat.tempresu - intc)
+    llik = -0.5 * np.sum(dist**2 / gdat.varidata**2)
+
+    return llik
+
+
 pathbase = os.environ['TDGU_DATA_PATH'] + '/pcurcorr/'
 pathdata = pathbase + 'data/'
 pathimag = pathbase + 'imag/'
 
-listnameresu = [r'$D_{TESS}$ [ppm]', '$D_{3.6}$ [ppm]', '$D_{4.5}$ [ppm]', '$T_D$ [K]', '$A_g$']
+# construct global object
+gdat = tdpy.util.gdatstrt()
+    
+listlablresu = [r'$D_{TESS}$ [ppm]', '$D_{3.6}$ [ppm]', '$D_{4.5}$ [ppm]', '$T_D$ [K]', '$A_g$']
 
 listdata = np.array([\
-            #['WASP-36 b'  , 120, 80 , 100 , 914 , 578 , 1953, 544 , 1420, 160, 180,   0., 0.52],\
-            ['WASP-43 b'  , 170, 70 , 80  , 3230, 60  , 3830, 80  , 1571, 44 , 43 , 0.13, 0.07],\
-            #['WASP-46 b'  , 110, 80 , 110 , 1360, 701 , 4446, 589 , 1870, 130, 130,   0., 0.40],\
-            #['WASP-64 b'  , 230, 110, 120 , 2859, 270 , 2071, 471 , 1960, 110, 110,   0., 0.76],\
-            ['WASP-77 A b', 55 , 18 , 20  , 2016, 94  , 2487, 127 , 1739, 30 , 31, 0.07, 0.03],\
-            #['WASP-78 b'  , 220, 85 , 84  , 2001, 218 , 2013, 351 , 2540, 150, 170,   0., 0.49],\
-            ['WASP-100 b' , 101, 13 , 13  , 1267, 98  , 1720, 119 , 2451, 89 , 83 , 0.20, 0.08],\
-            #['WASP-18 b'  , 341, 18 , 17  , 3037, 62  , 4033, 97  , 3037, 36 , 36 , 0., 0.04],\
+            ['WASP-36 b'  , 120, 80 , 100 , 914 , 578 , 1953, 544 , 1420, 160, 180,   0.23, 0.19],\
+            ['WASP-43 b'  , 170, 70 , 80  , 3230, 60  , 3830, 80  , 1571, 44 , 43 , 0.14, 0.07],\
+            #['WASP-46 b'  , 110, 80 , 110 , 1360, 701 , 4446, 589 , 1870, 130, 130,   0., 0.43],\
+            ['WASP-64 b'  , 230, 110, 120 , 2859, 270 , 2071, 471 , 1960, 110, 110,   0.39, 0.24],\
+            ['WASP-77 A b', 55 , 18 , 20  , 2016, 94  , 2487, 127 , 1739, 30 , 31, 0.06, 0.04],\
+            #['WASP-78 b'  , 220, 85 , 84  , 2001, 218 , 2013, 351 , 2540, 150, 170,   0., 0.65],\
+            ['WASP-100 b' , 101, 13 , 13  , 1267, 98  , 1720, 119 , 2451, 89 , 83 , 0.26, 0.07],\
+            
+            #['WASP-18 b'  , 341, 18 , 17  , 3037, 62  , 4033, 97  , 3037, 36 , 36 , 0., 0.02],\
             ['WASP-19 b'  , 101, 106, 131 , 5015,175  , 5343., 318 , 2174, 55 , 55 , 0.19, 0.08],\
-            ['WASP-121 b' , 101, 43 , 42  , 3685, 114 , 4684, 121 , 2577, 59 , 63 , 0.24, 0.04],\
+            ['WASP-121 b' , 101, 43 , 42  , 3685, 114 , 4684, 121 , 2577, 59 , 63 , 0.27, 0.04],\
             ])
 #arryresu = listdata[:, np.array([1, 4, 6, 8, 11])].astype(float)
 
-listnameresu = listnameresu[4:5]
+listlablresu = listlablresu[4:5]
+arryresustdv = listdata[:, np.array([12])].astype(float)
 arryresu = listdata[:, np.array([11])].astype(float)
 
 # look into the Exoplanet Archive to get the mass and radii of planets
@@ -66,31 +94,64 @@ indxcolsflot = np.array(indxcolsflot)
 arryinpt = arryinpt[:, indxcolsflot]
 listnameinpt = objtarch.columns[indxcolsflot]
 
+indxinptmeta = np.where(listnameinpt == 'fst_met')[0]
+
+arryinpt = arryinpt[:, indxinptmeta]
+listnameinpt = listnameinpt[indxinptmeta]
+
+listlablinpt = ['Fe/H']
+
+
 numbcompinpt = arryinpt.shape[1]
 indxcompinpt = np.arange(numbcompinpt)
 
-numbsamp = 1
-indxsamp = np.arange(numbsamp)
+coef = np.empty((numbcompinpt, numbcompresu))
+pval = np.empty((numbcompinpt, numbcompresu))
 
-coef = np.empty((numbsamp, numbcompinpt, numbcompresu))
-pval = np.empty((numbsamp, numbcompinpt, numbcompresu))
+print('numbcompresu')
+print(numbcompresu)
+
+numbsampwalk = 1000
+numbsampburnwalk = 200
+listlablpara = [['a', ''], ['b', '']]
+listscalpara = ['self', 'self']
+listminmpara = [-100., -100.]
+listmaxmpara = [100., 100.]
+listmeangauspara = None
+liststdvgauspara = None
+
+boolstan = False
 
 # find the p value
 numbtest = numbcompinpt * numbcompresu
-for i in indxsamp:
-    arryinpttemp = arryinpt * (np.random.randn() * 0.1 + 1.)
-    arryresutemp = arryresu * (np.random.randn() * 0.1 + 1.)
-    for n in indxcompinpt: 
-        for k in indxcompresu:
-            stdvinpt = np.std(arryinpttemp[:, n])
-            stdvresu = np.std(arryresutemp[:, k])
-            if stdvinpt < 1e-6 or stdvresu < 1e-6:
-                coef[i, n, k] = 0.
-                pval[i, n, k] = 2.
-            else:
-                tempinpt = (arryinpttemp[:, n] - np.mean(arryinpttemp[:, n])) / stdvinpt
-                tempresu = (arryresutemp[:, k] - np.mean(arryresutemp[:, k])) / stdvresu
-                coef[i, n, k], pval[i, n, k] = scipy.stats.pearsonr(tempinpt, tempresu)
+for n in indxcompinpt: 
+    for k in indxcompresu:
+        gdat.tempinptstdv = np.std(arryinpt[:, n])
+        gdat.tempresustdv = np.std(arryresu[:, k])
+        if gdat.tempinptstdv < 1e-6 or gdat.tempresustdv < 1e-6:
+            coef[n, k] = 0.
+            pval[n, k] = 2.
+        else:
+            gdat.tempinpt = arryinpt[:, n]
+            gdat.tempresu = arryresu[:, k]
+                
+            gdat.varidata = gdat.tempinptstdv**2 + gdat.tempresustdv**2
+
+            coef[n, k], pval[n, k] = scipy.stats.pearsonr(gdat.tempinpt, gdat.tempresu)
+           
+            numbdata = 2 * gdat.tempinpt.size
+            parapost = tdpy.mcmc.samp(gdat, pathimag, numbsampwalk, numbsampburnwalk, retr_llik, \
+                                listlablpara, listscalpara, listminmpara, listmaxmpara, listmeangauspara, liststdvgauspara, numbdata)
+            
+            numbsampfeww = 1000
+            numbsamp = parapost.shape[0]
+            indxsamp = np.arange(numbsamp)
+            indxsampplot = np.random.choice(indxsamp, size=numbsampfeww)
+            listmodl = np.empty((numbsampfeww, 100))
+            inpteval = np.linspace(-0.4, 0.3, 100)
+            for i in np.arange(numbsampfeww):
+                listmodl[i, :], _ = retr_modl(gdat, parapost[i, :], inpteval)
+            
 pval = np.mean(pval, 0)
 coef = np.mean(coef, 0)
 
@@ -105,21 +166,32 @@ indxresusort = indxcompresumesh.flatten()[indxsort]
 for a in range(indxsort.size):
     if a == 100:
         break
-    titl = 'p = %g, C = %g, %s %s' % (pval.flatten()[indxsort[a]], coef.flatten()[indxsort[a]], listnameinpt[indxinptsort[a]], listnameresu[indxresusort[a]])
+    titl = 'p = %g, C = %g, %s %s' % (pval.flatten()[indxsort[a]], coef.flatten()[indxsort[a]], listlablinpt[indxinptsort[a]], listlablresu[indxresusort[a]])
     indxxpos = indxinptsort[a]
     indxypos = indxresusort[a]
     if pval.flatten()[indxsort[a]] != 2. and (np.std(arryinpt[:, indxinptsort[a]]) < 1e-6 or np.std(arryresu[:, indxresusort[a]]) < 1e-6):
         raise Exception('')
     figr, axis = plt.subplots(figsize=(5, 5))
-    if listnameresu[indxresusort[a]] == '$A_g$':
+    if listlablresu[indxresusort[a]] == '$A_g$':
         yerr = listdata[:, 12].astype(float)
         xerr = None
     else:
         yerr = None
         xerr = None
-    axis.errorbar(arryinpt[:, indxinptsort[a]], arryresu[:, indxresusort[a]], yerr=yerr, xerr=xerr, fmt='o')
-    axis.set_xlabel(listnameinpt[indxinptsort[a]])
-    axis.set_ylabel(listnameresu[indxresusort[a]])
+    for i in np.arange(numbsampfeww):
+        axis.plot(inpteval, listmodl[i, :], color='b', alpha=0.03)
+    
+    path = pathdata + 'data_%02d.csv' % a
+    arry = np.empty((arryinpt.shape[0], 3))
+    arry[:, 0] = arryinpt[:, indxinptsort[a]]
+    arry[:, 1] = arryresu[:, indxresusort[a]]
+    arry[:, 2] = yerr
+    print('Writing to %s...' % path)
+    np.savetxt(path, arry, delimiter=',')
+
+    axis.errorbar(arryinpt[:, indxinptsort[a]], arryresu[:, indxresusort[a]], yerr=yerr, xerr=xerr, fmt='o', color='k')
+    axis.set_xlabel(listlablinpt[indxinptsort[a]])
+    axis.set_ylabel(listlablresu[indxresusort[a]])
     axis.set_title(titl)
     plt.tight_layout()
     path = pathimag + 'scat_%02d.pdf' % (a)
--- toii1233/main.py
+++ toii1233/main.py
@@ -1,23 +1,27 @@
-import tesstoii.main
+import pexo.main
 import numpy as np
 
 strgtarg = 'TOI1233'
-strgmast = '260647166'
+ticitarg = 260647166
+strgmast = 'TIC' + str(ticitarg)
 labltarg = 'TOI 1233'
 
-strgtoii = '1233'
+inclprio = np.array([90., 90., 90., 90.])
+strgtoii = ['1233.01', '1233.02', '1233.03', '1233.04']
 
 massstar = 0.97 # [M_S]
 radistar = 0.888 # [R_S]
 
-tesstoii.main.main( \
-                   #strgdata='qlop', \
+pexo.main.main( \
                    strgtarg=strgtarg, \
                    labltarg=labltarg, \
                    
                    strgtoii=strgtoii, \
                    strgmast=strgmast, \
+                   ticitarg=ticitarg, \
                    
+                   inclprio=inclprio, \
+
                    massstar=massstar, \
                    radistar=radistar, \
                     
